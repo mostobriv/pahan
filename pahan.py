72f28161197159b12fe038d4e30c113bcbd7d034
Obriv Mostov <mostobriv@MacBook-Pro-Obriv.local>
2019-08-29 22:48:23 +0700

    bla bla bla

--- pahan.py
+++ pahan.py
@@ -1,17 +1,6 @@
 from traffic.configurator import Configurator
 from traffic.masta import Masta
 
-
-# def main():
-    # traffic_slicer = Slicer()
-    # traffic_crawler = Crawler(os.path.abspath('dumps'))
-    # task1 = traffic_slicer.slice_pcap_tshark('dumps/small_pcap_file.pcap')
-    # task2 = traffic_crawler.crawl()
-
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(asyncio.wait([task1, task2]))
-    # loop.close()
-
 if __name__ == '__main__':
     m = Masta.from_configurator(Configurator())
     m.run()

f5b7c3f09d0721c13cf20eadb5797f043739d72b
mostobriv <mostobriv@gmail.com>
2019-08-29 16:52:33 +0700

    added abstract classes

--- traffic/crawler.py
+++ traffic/crawler.py
@@ -1,15 +1,26 @@
 import glob
 import os.path
 from traffic.logger import Logger
+import asyncio
 
 
-class Crawler:
+# thx to @andgein for the idea of abstract classes <3
+class AbstractCrawler:
+    def __init__(self):
+        self._logger = Logger('Crawler')
+
+    async def crawl(self):
+        raise NotImplementedError()
+
+
+class Crawler(AbstractCrawler):
 
     def __init__(self, directory: str):
-        self._logger = Logger(self)
+        super().__init__()
         self.directory = directory
+    
 
-    def crawl(self):
+    async def crawl(self):
         self._logger.info('Crawling into %s' % os.path.abspath(self.directory))
-        for file_name in glob.glob('%s/**/*.pcap' % self.directory, recursive=True):
+        for file_name in glob.glob('%s/**/*.parts/*.pcap' % self.directory, recursive=True):
             self._logger.debug(f'Found pcap: {file_name}')
--- traffic/slicer.py
+++ traffic/slicer.py
@@ -1,16 +1,27 @@
 from traffic.logger import Logger
 import time
-import os
+import asyncio
 
-class Slicer:
+
+# thx to @andgein for the idea of abstract classes <3
+class AbstractSlicer:
+    def __init__(self):
+        self._logger = Logger('Slicer')
+
+    async def slice_pcap(self, path: str):
+        raise NotImplementedError()
+
+
+class Slicer(AbstractSlicer):
 
     def __init__(self):
-        self._logger = Logger(self)
+        super().__init__()
 
-    def slice_pcap_tshark(self, path: str):
+    async def slice_pcap(self, path: str):
         self._logger.debug(f'Started slicing of {path}')
         try:
-            os.system(f"tshark -q -X lua_script:tcp-stream-splitter.lua -X lua_script1:{path} -n -r {path}")
+            proc = await asyncio.create_subprocess_shell(f"tshark -q -X lua_script:tcp-stream-splitter.lua -X lua_script1:{path} -n -r {path}")
+            retcode = await proc.wait()
         except Exception as e:
             self._logger.error(f'Got an exception {e}', e)
-        self._logger.debug(f'Finished slicing of {path}')
+        self._logger.debug(f'Finished slicing of {path} with {retcode} return status code')

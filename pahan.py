e7a75ded0a3ba3b67d5bf8937f46cb023b6d17ac
mostobriv <mostobriv@gmail.com>
2019-08-29 16:52:07 +0700

    restructured

--- pahan.py
+++ pahan.py
@@ -1,18 +1,17 @@
-import os
-# from web import app
-from traffic.slicer import Slicer
-from traffic.crawler import Crawler
-# import configrurator
+from traffic.configurator import Configurator
+from traffic.masta import Masta
 
 
-def main():
-    # web_task            = WebApp()
-    traffic_slicer = Slicer()
-    traffic_crawler = Crawler(os.path.abspath('dumps'))
-    traffic_slicer.slice_pcap_tshark('dumps/small_pcap_file.pcap')
-    # traffic_slicer.slice_pcap_tshark('dumps/big_pcap_file.pcap')
-    traffic_crawler.crawl()
-    # traffic_crawler     = traffic_crawler.from_configurator(Configurator())
+# def main():
+    # traffic_slicer = Slicer()
+    # traffic_crawler = Crawler(os.path.abspath('dumps'))
+    # task1 = traffic_slicer.slice_pcap_tshark('dumps/small_pcap_file.pcap')
+    # task2 = traffic_crawler.crawl()
+
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(asyncio.wait([task1, task2]))
+    # loop.close()
 
 if __name__ == '__main__':
-    main()
+    m = Masta.from_configurator(Configurator())
+    m.run()

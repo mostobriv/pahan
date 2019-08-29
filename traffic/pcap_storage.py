02380e6dd339e7d78665da5c5396ea49e67a38a9
mostobriv <mostobriv@gmail.com>
2019-08-29 18:35:23 +0700

    class for handling stuff related to storing pcaps

---
+++ traffic/pcap_storage.py
@@ -0,0 +1,23 @@
+from traffic.logger import Logger
+
+import os
+import glob
+
+
+class AbstractPcapStorage:
+
+    def __init__(self):
+        self._logger = Logger('PcapStorage')
+    
+    def get_list_of_pcaps(self):
+        raise NotImplementedError()
+
+
+class DirectoryPcapStorage(AbstractPcapStorage):
+
+    def __init__(self, directory: str):
+        super().__init__()
+        self.directory = directory
+
+    def get_list_of_pcaps(self):
+        return glob.iglob('%s/**/[^.]*.pcap' % self.directory, recursive=True)

a771a2c255bfcb9758bfbb7fa6ec05b286c00282
mostobriv <mostobriv@gmail.com>
2019-08-29 16:53:07 +0700

    added to make managing of loop and stuff easier

---
+++ traffic/masta.py
@@ -0,0 +1,13 @@
+from traffic.configurator import Configurator
+
+
+class Masta:
+
+    def __init__(self, ):
+        self.slicer =
+
+    @classmethod
+    def from_configurator(cls, configurator: Configurator):
+        return cls(
+
+        )
